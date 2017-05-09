5f9702d3d6e42869c6750ba33e031fcfb76b792d
Merijn Verstraaten <merijn@inconsistent.nl>
2017-05-09 15:33:36 +0200

    Made channels closable and add more expressive exceptions.

--- Control/Concurrent/BroadcastChan.hs
+++ Control/Concurrent/BroadcastChan.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE AutoDeriveTypeable #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE Safe #-}
 -------------------------------------------------------------------------------
 -- |
@@ -28,17 +28,32 @@
 -- garbage collected if there are no active read ends, avoding space leaks.
 -------------------------------------------------------------------------------
 module Control.Concurrent.BroadcastChan
-    ( BroadcastChan
+    ( BChanError
+    , BroadcastChan
     , In
     , Out
     , newBroadcastChan
+    , closeBChan
+    , isClosedBChan
     , writeBChan
+    , writeBChan_
     , readBChan
+    , readBChan_
     , newBChanListener
     ) where
 
+import Control.Monad (when)
 import Control.Concurrent.MVar
-import Control.Exception (mask_)
+import Control.Exception (Exception, mask_, throwIO)
+import GHC.Generics (Generic)
+
+-- | Exception type for 'BroadcastChan' operations.
+data BChanError
+    = WriteFailed   -- ^ Attempted to write to closed 'BroadcastChan'
+    | ReadFailed    -- ^ Attempted to read from closed 'BroadcastChan'
+    deriving (Eq, Read, Show)
+
+instance Exception BChanError
 
 data Direction = In | Out
 
@@ -51,11 +66,11 @@ type Out = 'Out
 
 -- | The abstract type representing the read or write end of a 'BroadcastChan'.
 newtype BroadcastChan (d :: Direction) a = BChan (MVar (Stream a))
-    deriving (Eq)
+    deriving (Eq, Generic)
 
 type Stream a = MVar (ChItem a)
 
-data ChItem a = ChItem a {-# UNPACK #-} !(Stream a)
+data ChItem a = ChItem a {-# UNPACK #-} !(Stream a) | Closed
 
 -- | Creates a new 'BroadcastChan' write end.
 newBroadcastChan :: IO (BroadcastChan In a)
@@ -64,25 +79,71 @@ newBroadcastChan = do
    writeVar <- newMVar hole
    return (BChan writeVar)
 
+-- | Close a 'BroadcastChan', disallowing further writes. Return value
+-- indicates whether the 'BroadcastChan' was already closed.
+closeBChan :: BroadcastChan In a -> IO Bool
+closeBChan (BChan writeVar) = mask_ $ do
+    old_hole <- takeMVar writeVar
+    tryPutMVar old_hole Closed <* putMVar writeVar old_hole
+
+-- | Check whether a 'BroadcastChan' is closed. Beware of TOC-TOU races,
+-- it is possible for a 'BroadcastChan' to be closed by another thread. If
+-- multiple threads use the same 'BroadcastChan' a 'closeBChan' from another
+-- thread might cause writes to fail even after 'isClosedBChan' returns 'True'.
+isClosedBChan :: BroadcastChan In a -> IO Bool
+isClosedBChan (BChan writeVar) = mask_ $ do
+    old_hole <- takeMVar writeVar
+    val <- tryReadMVar old_hole
+    case val of
+        Just Closed -> return True
+        _ -> return False
+
 -- | Write a value to write end of a 'BroadcastChan'. Any messages written
 -- while there are no live read ends can be immediately garbage collected, thus
 -- avoiding space leaks.
-writeBChan :: BroadcastChan In a -> a -> IO ()
+--
+-- The return value indicates whether the write succeeded or failed (due to a
+-- closed 'BroadcastChan'). See 'writeBChan_' for an exception throwing
+-- variant.
+writeBChan :: BroadcastChan In a -> a -> IO Bool
 writeBChan (BChan writeVar) val = do
   new_hole <- newEmptyMVar
   mask_ $ do
     old_hole <- takeMVar writeVar
-    putMVar old_hole (ChItem val new_hole)
-    putMVar writeVar new_hole
+    empty <- tryPutMVar old_hole (ChItem val new_hole)
+    if empty
+       then putMVar writeVar new_hole
+       else putMVar writeVar old_hole
+    return empty
 
--- | Read the next value from the read end of a 'BroadcastChan'.
-readBChan :: BroadcastChan Out a -> IO a
+-- | Like 'writeBChan', but throws a 'WriteFailed' exception when writing to
+-- closed 'BroadcastChan'.
+writeBChan_ :: BroadcastChan In a -> a -> IO ()
+writeBChan_ ch val = do
+    success <- writeBChan ch val
+    when (not success) $ throwIO WriteFailed
+
+-- | Read the next value from the read end of a 'BroadcastChan'. Returns
+-- 'Nothing' if the 'BroadcastChan' is closed and empty. See 'readBChan_' for
+-- an exception throwing variant.
+readBChan :: BroadcastChan Out a -> IO (Maybe a)
 readBChan (BChan readVar) = do
   modifyMVarMasked readVar $ \read_end -> do -- Note [modifyMVarMasked]
-    (ChItem val new_read_end) <- readMVar read_end
-        -- Use readMVar here, not takeMVar,
-        -- else dupBroadcastChan doesn't work
-    return (new_read_end, val)
+    -- Use readMVar here, not takeMVar,
+    -- else newBChanListener doesn't work
+    result <- readMVar read_end
+    case result of
+        (ChItem val new_read_end) -> return (new_read_end, Just val)
+        Closed -> return (read_end, Nothing)
+
+-- | Like 'readBChan', but throws a 'ReadFailed' exception when reading from a
+-- closed and empty 'BroadcastChan'.
+readBChan_ :: BroadcastChan Out a -> IO a
+readBChan_ ch = do
+    result <- readBChan ch
+    case result of
+        Nothing -> throwIO ReadFailed
+        Just x -> return x
 
 -- Note [modifyMVarMasked]
 -- This prevents a theoretical deadlock if an asynchronous exception
